f92bed61fd4e2debd6916695232b6fb3fde9146a
akadane87 <akadane87@gmail.com>
2016-10-24 13:08:04 -0400

    filled in code for files

--- bin/array-iteration-methods.js
+++ bin/array-iteration-methods.js
@@ -1,7 +1,15 @@
 'use strict';
 
+// const forEach = require('.../lib/for-each.js');
 const forEach = require('../lib/for-each.js');
 
+// const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const array = new Array(10).fill(1).map((e, i) => e * i);
 
 forEach(array, (e, i, a) => console.log(a === array));
+
+// const testArray = (e, i, a) => console.log(a === array);
+
+// forEach(array, testArray);
+
+let result = reduce(array, (previous, current) )
